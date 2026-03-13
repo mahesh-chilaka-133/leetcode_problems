044e2f7254372cfe43268e40e7ee339bdc18bf0a
mahesh-chilaka-133 <maheshchilaka13@gmail.com>
2026-03-13 20:46:48 +0530

    array leetcode solutions using prefixsum + hashmap and sliding window

---
+++ arrays/ContinuousSubarraySumGood.java
@@ -0,0 +1,30 @@
+package arrays;
+import java.util.*;
+public class ContinuousSubarraySumGood {
+    
+    public boolean checkSubarraySum(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);  
+        int prefixSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            prefixSum += nums[i];
+            int remainder = prefixSum % k;
+
+            if (map.containsKey(remainder)) {
+
+                if (i - map.get(remainder) > 1) {
+                    return true;
+                }
+
+            } else {
+                map.put(remainder, i);
+            }
+        }
+
+        return false;
+    }
+}
+
