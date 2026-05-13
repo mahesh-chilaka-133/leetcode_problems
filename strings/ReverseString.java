9e2960480e883cfc4d7d68d3c17bb80c27369c2b
mahesh-chilaka-133 <maheshchilaka13@gmail.com>
2026-05-13 12:17:27 +0530

    reverse string  optimal solution

---
+++ strings/ReverseString.java
@@ -0,0 +1,22 @@
+package strings;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+
+        int n = s.length;
+        int left = 0;
+        int right = n - 1;
+
+        while ( left  < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+
+
+    }
+
+}
