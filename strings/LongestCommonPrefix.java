8a31afc44cf4558036679f9a0d3a1b41de37d826
mahesh-chilaka-133 <maheshchilaka13@gmail.com>
2026-06-29 11:52:57 +0530

    Strings - patterns revising.java

---
+++ strings/LongestCommonPrefix.java
@@ -0,0 +1,28 @@
+package strings;
+
+public class LongestCommonPrefix {
+
+  
+    public String longestCommonPrefix(String[] strs) {
+
+        if (strs == null ||strs.length == 0 ) {
+            return "";
+        }
+
+        for (int i = 0; i < strs[0].length(); i++) {
+
+            char current = strs[0].charAt(i);
+
+            for ( int j = 1; j < strs.length; j++) {
+
+                if (i == strs[j].length() || strs[j].charAt(i) != current  ) {
+
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+        
+    }
+
+}
