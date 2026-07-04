6740704aef7b5eff29830b01102f34fb3743e7b0
mahesh-chilaka-133 <maheshchilaka13@gmail.com>
2026-07-04 11:36:43 +0530

    LinkedLists - iterative

---
+++ linkedlists/ReverseLinkedlist.java
@@ -0,0 +1,20 @@
+package linkedlists;
+
+public class ReverseLinkedlist {
+
+    public ListNode reverseList(ListNode head) {
+        
+        ListNode temp = head;
+        ListNode prev = null;
+        ListNode front;
+
+        while (temp != null) {
+
+            front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        } 
+        return prev;
+    }
+}
