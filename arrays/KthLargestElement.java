4d2ac262be8576a9d35489a6faab7e86a3ec4b00
mahesh-chilaka-133 <maheshchilaka13@gmail.com>
2026-08-25 21:01:37 +0530

    Arrays

---
+++ arrays/KthLargestElement.java
@@ -0,0 +1,19 @@
+
+package arrays;
+public class KthLargestElement {
+
+    public int findKthLargest(int[] nums, int k) {
+        
+        Queue<Integer> heap = new PriorityQueue<>();
+
+        for (int num: nums){
+            heap.offer(num);
+
+            if (heap.size() > k){
+                heap.poll();
+            }
+        }
+        return heap.peek();
+    }
+}
+
